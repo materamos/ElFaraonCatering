e7d1c5b3c675a7c276e7fe16ffe03f3723a7d808
materamos <mate.rojo.02@gmail.com>
2026-04-21 14:55:58 -0300

    chore: remove decap and legacy admin tooling

--- public/admin/admin-identity.js
+++
@@ -1,9 +0,0 @@
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", (user) => {
-    if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        window.location.href = "/admin/";
-      });
-    }
-  });
-}
